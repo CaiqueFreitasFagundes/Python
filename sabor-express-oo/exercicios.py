29b09b7024fd795efd801d345e5431380045f643
Caique Freitas Fagundes <119082350+CaiqueFreitasFagundes@users.noreply.github.com>
2024-02-07 21:43:38 -0300

    Create exercicios.py

---
+++ sabor-express-oo/exercicios.py
@@ -0,0 +1,20 @@
+# Exercicio sobre Classes
+
+# Crie uma classe Música poderia ter 3 atributos que trazem as características ou propriedades de um objeto
+
+class musica:
+    nome = ''
+    artista = ''
+    duracao = 0 # em segundos
+
+musica_1 = musica()
+musica_1.nome = 'Ainda é cedo'
+musica_1.artista = 'Legião Urbana'
+musica_1.duracao = 180
+
+musica_2 = musica()
+musica_2.nome = 'Tempo Perdido'
+musica_2.artista = 'Legião Urbana'
+musica_2.duracao = 240
+
+print(musica_1.artista)
