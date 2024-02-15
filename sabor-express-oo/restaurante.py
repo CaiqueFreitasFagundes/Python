c418b1ffae35d4276d79f1af73cfde210899a81d
Caique Freitas Fagundes <119082350+CaiqueFreitasFagundes@users.noreply.github.com>
2024-02-14 21:23:47 -0300

    Update restaurante.py

--- sabor-express-oo/restaurante.py
+++ sabor-express-oo/restaurante.py
@@ -1,14 +1,19 @@
-class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+class Restaurante:#Classe sempre com a primeira letra maiúscula.
+    restaurantes = []
+    def __init__(self, nome, categoria):#Método construtor, que é chamado quando um objeto é instanciado. O self é uma referência ao próprio objeto.
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __str__(self):#Método para retornar uma string com as informações do objeto, quando o objeto é passado como parâmetro para a função print. 
+        return f'Nome: {self.nome} - Categoria: {self.categoria} - Ativo: {self.ativo}'
+    
+    def listar_restaurantes():#Método estático para listar os restaurantes cadastrados. Não recebe o self como parâmetro, pois não é um método de instância.
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} - {restaurante.categoria} - {restaurante.ativo}')
 
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Fast Food')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(dir(restaurante_praca))
+Restaurante.listar_restaurantes()
