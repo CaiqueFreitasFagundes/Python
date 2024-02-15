9494ac262f9382dbd8ed99946e0b3c8a4aa16a43
Caique Freitas Fagundes <119082350+CaiqueFreitasFagundes@users.noreply.github.com>
2024-02-15 20:56:35 -0300

    Update exercicios.py

--- sabor-express-oo/exercicios.py
+++ sabor-express-oo/exercicios.py
@@ -19,4 +19,16 @@ musica_2.duracao = 240
 
 print(musica_1.artista)
 
-# 
+#Outra maneira de fazer a mesma coisa é utilizando o método construtor __init__:
+
+class Musica:
+    def __init__(self, nome='', artista='', duracao=0):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+
+musica1 = Musica(nome='Chop Suey', artista='System of a Down', duracao=180)
+musica2 = Musica(nome='Toxicity', artista='System of a Down', duracao=240)
+musica3 = Musica(nome='Guajira', artista='Yerba Buena', duracao=300)
+
+print(musica1.artista)
