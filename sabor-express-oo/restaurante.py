09ed22e00453a2d97d9d6096f431844f4c8d9069
Caique Freitas Fagundes <119082350+CaiqueFreitasFagundes@users.noreply.github.com>
2024-02-26 20:46:11 -0300

    Update restaurante.py

--- sabor-express-oo/restaurante.py
+++ sabor-express-oo/restaurante.py
@@ -4,20 +4,31 @@ class Restaurante:#Classe sempre com a primeira letra maiúscula.
         
         # O Self é uma referência ao objeto que está sendo criado, ele é usado para acessar os atributos e métodos do objeto. 
 
-        self.nome = nome #self.nome é chamado, pois queremos acessar o atributo nome do objeto que está sendo criado, e além de acessar, damos o valor nome a ele, que foi passado como parâmetro na criação do objeto, lá no __init__. Assim, quando o objeto é criado, ele já é criado com o atributo nome preenchido.
-        self.categoria = categoria #O mesmo acontece com a categoria.     
-        self.ativo = False 
+        self._nome = nome.title() #self.nome é chamado, pois queremos acessar o atributo nome do objeto que está sendo criado, e além de acessar, damos o valor nome a ele, que foi passado como parâmetro na criação do objeto, lá no __init__. Assim, quando o objeto é criado, ele já é criado com o atributo nome preenchido. Foi modificado para self._nome para que o atributo seja privado.
+        
+        self._categoria = categoria.upper() #O mesmo acontece com a categoria.   
+
+        self._ativo = False 
+
         Restaurante.restaurantes.append(self) #Adiciona o objeto à lista de restaurantes.
 
     def __str__(self):#Método para retornar uma string com as informações do objeto, quando o objeto é passado como parâmetro para a função print. 
-        return f'Nome: {self.nome} - Categoria: {self.categoria} - Ativo: {self.ativo}'
+        return f'Nome: {self._nome} | Categoria: {self._categoria}'
     
     def listar_restaurantes():#Método estático para listar os restaurantes cadastrados. Não recebe o self como parâmetro, pois não é um método de instância.
+
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} - {restaurante.categoria} - {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property #o property é um decorador que permite que o método seja chamado como um atributo, sem a necessidade de usar os parênteses. Ele oferece a possibilidade de criar um método que se comporta como um atributo.
+
+    def ativo(self):
+        return '✔️' if self._ativo else '❌'
 
 # Instanciando a classe Restaurante
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Fast Food')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('pizza Express', 'Italiana')
+restaurante_pizza._ativo = True
 
 Restaurante.listar_restaurantes()
