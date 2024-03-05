6a7c82a39a29c10df2555a267348b32152c2f0ba
Caique Freitas Fagundes <119082350+CaiqueFreitasFagundes@users.noreply.github.com>
2024-03-05 19:16:47 -0300

    Update exercicios.py

--- sabor-express-oo/exercicios.py
+++ sabor-express-oo/exercicios.py
@@ -103,6 +103,8 @@ print(vars(novo_cadastro))
 print(vars(novo_cadastro2))
 print(vars(novo_cadastro3))
 
+
+# -------------------------------------------------------------------
 class Livro:
     def __init__(self, titulo='', autor='', paginas=0):
         self.titulo = titulo
@@ -120,3 +122,23 @@ class Livro:
         self.paginas += quantidade
 
 # Agora é sua vez! Crie uma nova classe chamada Pessoa com atributos como nome, idade e profissão. Adicione um método especial __str__ para imprimir uma representação em string da pessoa. Implemente também um método de instância chamado aniversario que aumenta a idade da pessoa em um ano. Por fim, adicione uma propriedade chamada saudacao que retorna uma mensagem de saudação personalizada com base na profissão da pessoa.
+        
+class Pessoa:
+    def __init__(self, nome = '', idade=0, profissao=''):
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
+    
+    def __str__(self):
+        return f'Olá meu nome é {self.nome} e tenho {self.idade} anos, minha profissão é {self.profissao}!'
+    
+    @property
+    def nome_idade(self):
+        return f'{self.nome} por {self.idade}'
+    
+    def aniversario(self, anos):
+        self.idade += anos
+
+pessoa1 = Pessoa(nome = 'Caique', idade = 31, profissao = 'TI')
+
+print(pessoa1)
